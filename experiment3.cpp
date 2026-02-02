e63d2f0a002ab6119fb9b54fa8c75b65bed32bce
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-02-02 14:46:41 +0530

    Add files via upload

---
+++ experiment3.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<char> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    unordered_map<int, int> mp;
+    mp[0] = -1;
+
+    int sum = 0;
+    int maxLen = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 'P')
+        {
+            sum += 1;
+        }
+        else
+        {
+            sum -= 1;
+        }
+
+        if (mp.find(sum) != mp.end())
+        {
+            int len = i - mp[sum];
+            if (len > maxLen)
+            {
+                maxLen = len;
+            }
+        }
+        else
+        {
+            mp[sum] = i;
+        }
+    }
+
+    cout << maxLen << endl;
+}
